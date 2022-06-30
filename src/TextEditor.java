d619b4e16f0a460b6fd2026a968a12b7aaecf426
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-06-30 19:39:45 -0300

    New features

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,13 +1,36 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
-    protected TextEditor() {
+
+    JTextArea textArea;
+    ImageIcon logo = new ImageIcon("src/images/nota.png");
+
+    public TextEditor() {
+        createTextArea();
+        createFrame();
+    }
+
+    private void createTextArea() {
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+    }
+
+    private void createFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("MyNotePad");
         this.setSize(1000, 700);
         this.setLocationRelativeTo(null);
+        this.setIconImage(logo.getImage());
+        addFrame();
         this.setVisible(true);
+    }
+
+    private void addFrame() {
+        this.add(textArea);
     }
 }
