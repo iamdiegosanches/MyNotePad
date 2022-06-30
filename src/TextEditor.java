dd10396f41761004f765a764e8d686304173dce0
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-06-30 19:24:21 -0300

    First panel creation

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,4 +1,13 @@
 package src;
 
-public class TextEditor {
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+    protected TextEditor() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("MyNotePad");
+        this.setSize(1000, 700);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
